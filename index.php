95be6eabbde9481eaac36aca0695f54d774657a5
jrmunozv <jrmunozv@yahoo.com>
2021-05-22 20:53:06 -0400

    First clean version

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-
-require 'vendor/autoload.php';
-
-require_once 'plantillas/reporte/index.php';
-
-$css= file_get_contents('plantillas/reporte/style.css');
-
-
-$mpdf= new \Mpdf\Mpdf([
-
-]);
-
-$plantilla= getPlantilla();
-
-$mpdf->writeHtml($css,\Mpdf\HTMLParserMode::HEADER_CSS);
-$mpdf->writeHtml($plantilla,\Mpdf\HTMLParserMode::HTML_BODY);
-
-
-$mpdf->Output();
+require_once("view/index.phtml");
 
 ?>
